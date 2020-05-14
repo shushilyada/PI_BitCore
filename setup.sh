0bbf6278bdbc43ee6aabea98a0d926f2bc7c8b50
SpecTurrican <lueths@gmail.com>
2020-05-14 21:44:15 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ else
 COIN="BitCore"
 SETUP=$(echo ${COIN} | tr '[:upper:]' '[:lower:]')
 OS=$(cat /etc/os-release | grep ID=raspbian)
+OS_Version=$(cat /etc/debian_version | grep 10)
 GIT_URL="https://github.com/SpecTurrican/PI_${COIN}"
 INSTALL_DIR="/root/PI_${COIN}/"
 INSTALL_FILE="${INSTALL_DIR}${SETUP}_setup/install_${SETUP}.sh"
@@ -18,18 +19,28 @@ LOG_FILE="start.log"
 apt-get -y update && apt-get -y install git
 
 	if [ -n "$OS" ]; then
-
+		if [ -n "OS_Version" ]; then
+		
 		cd /root/
 		git clone ${GIT_URL}
 		chmod 744 -R ${INSTALL_DIR}
 		mkdir ${LOG_DIR}
 		nohup ${INSTALL_FILE} >${LOG_DIR}${LOG_FILE} 2>&1 &
 		clear
+		
 		tail -f ${LOG_DIR}${LOG_FILE}
+
+		else
+
+			echo "This script running only below raspian buster... sorry !!!"
+			echo " "
+			echo "Visit https://www.raspberrypi.org/downloads/raspbian/ "
+
+		fi
 
 	else
 
-		echo "This script running only below raspian ... sorry !!!"
+		echo "This script running only below raspian buster... sorry !!!"
 		echo " "
 		echo "Visit https://www.raspberrypi.org/downloads/raspbian/ "
 
