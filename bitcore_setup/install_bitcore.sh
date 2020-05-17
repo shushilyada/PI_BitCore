9dba0c901bf771656055d32b5b01208d4f10e2e5
SpecTurrican <lueths@gmail.com>
2020-05-17 16:16:12 +0200

    Update install_bitcore.sh

--- bitcore_setup/install_bitcore.sh
+++ bitcore_setup/install_bitcore.sh
@@ -16,7 +16,7 @@ COIN_NODE="https://chainz.cryptoid.info/btx/api.dws?q=nodes"
 # DIRS
 ROOT="/root/"
 INSTALL_DIR="${ROOT}PI_${COIN_NAME}/"
-COIN_ROOT="/home/${COIN}.${COIN}"
+COIN_ROOT="/home/${COIN}/.${COIN}"
 COIN_INSTALL="${ROOT}${COIN}"
 BDB_PREFIX="${COIN_INSTALL}/db4"
 
